c395b81d6279fe458b5225b3831f290436117ea0
Alexander <malinovskiy-alex@ukr.net>
2014-09-20 14:57:42 +0300

    Added oportunity to run selenium tests.

---
+++ src/test/java/com/malinovskiy/barcodereader/integration/BarcodesPageIT.java
@@ -0,0 +1,23 @@
+package com.malinovskiy.barcodereader.integration;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by malinovsky on 9/20/2014.
+ */
+public class BarcodesPageIT extends TestCase {
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    public void testWeSeeHelloWorld() throws InterruptedException {
+        Thread.sleep(5000);
+    }
+}
